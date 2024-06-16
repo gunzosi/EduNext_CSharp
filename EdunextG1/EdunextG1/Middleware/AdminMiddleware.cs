1135434e9a913979076783e7f72458a32b746728
Dang Dai Duong <127579534+gunzosi@users.noreply.github.com>
2024-06-16 16:11:55 +0700

    Middleware for User role Admin
    The Admin Middleware

---
+++ EdunextG1/EdunextG1/Middleware/AdminMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace EdunextG1.Middleware
+{
+    public class AdminMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AdminMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/api/admin") 
+                && !context.User.IsInRole("Admin"))
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync("Forbidden: Only admins are allowed to access this resource.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
